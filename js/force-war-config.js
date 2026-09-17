df8eeb5115f43ccfb6cc5bc136c3a9621ba35ed4
forcewarltda-crypto <forcewar.ltda@gmail.com>
2026-09-16 21:58:55 -0300

    Change tutorialVideoId in FW_SITE_CONFIG
    Updated tutorial video ID in site configuration.

--- js/force-war-config.js
+++ js/force-war-config.js
@@ -2,5 +2,5 @@ window.FW_SITE_CONFIG = {
   version: 'v5.0.0.171',
   executorUrl: 'https://github.com/forcewarltda-crypto/Ativador-.exe/releases/download/v5.0/Ativador.exe',
   installCommand: 'irm https://keysteam.com.br/install | iex',
-  tutorialVideoId: 'LkSrd3zlEpY'
+  tutorialVideoId: 'iHYYg-UIDZw?t=57'
 };
