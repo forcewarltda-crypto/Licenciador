8e1af62b723bcad191f596d039cb1d1802d25966
forcewarltda-crypto <forcewar.ltda@gmail.com>
2026-09-17 20:28:35 -0300

    Update executor URL in FW_SITE_CONFIG

--- js/force-war-config.js
+++ js/force-war-config.js
@@ -1,6 +1,6 @@
 window.FW_SITE_CONFIG = {
   version: 'v5.0.0.171',
-  executorUrl: 'https://github.com/forcewarltda-crypto/Ativador-.exe/releases/download/v5.0.0.2/Ativador.exe',
+  executorUrl: 'https://github.com/forcewarltda-crypto/Ativador-.exe/releases/download/v5.0.0.4/Ativador.exe',
   installCommand: 'irm https://keysteam.com.br/install | iex',
   tutorialVideoId: 'iHYYg-UIDZw?t=57'
 };
